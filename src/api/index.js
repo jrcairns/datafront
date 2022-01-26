f360478385ded302804c4811148cf3e61f81ee11
jrcairns <cairns.webdev@gmail.com>
2022-01-26 17:19:02 -0500

    gitignore .env, update api baseURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_ENDPOINT
+  baseURL: `${process.env.REACT_APP_API_ENDPOINT}/api`
 })
 
 export const API_KEYS = {
