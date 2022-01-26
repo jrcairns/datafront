f3ebe9c998b455d852c438fb2431868e80dd0397
jrcairns <cairns.webdev@gmail.com>
2022-01-26 15:38:25 -0500

    Set up routes and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom"
+
+// Pages
+import LoginPage from './pages/Login'
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </div>
+  )
 }
